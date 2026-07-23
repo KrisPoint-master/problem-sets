526489119916ff9f094ba4d29d7948dfbb88cdd6
KrisPoint <KrisPointCSGO@outlook.com>
2026-07-23 14:50:52 +0800

    fix(problem/luogu/P9952) : complete P9952

--- problem/luogu/P9952/bin_search.cpp
+++ problem/luogu/P9952/bin_search.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int MAXN = 1000005;
 int n;
-char a[1000005];
+string a;
 
 int mid(int l, int r)
 {
@@ -11,6 +12,29 @@ int mid(int l, int r)
 
 bool check(int val)
 {
+    if (val == 0)
+        return false;
+    vector<int> pos;
+    for (int i = 1; i <= n; i++)
+        if (a[i - 1] == '1')
+            pos.push_back(i);
+
+    if (pos.empty())
+        return val > n - 1;
+
+    for (int i = 1; i < (int)pos.size(); i++)
+        if (pos[i] - pos[i - 1] < val)
+            return true;
+
+    int cnt = 0;
+    cnt += (pos[0] - 1) / val;
+    cnt += (n - pos.back()) / val;
+    for (int i = 1; i < (int)pos.size(); i++)
+    {
+        int len = pos[i] - pos[i - 1];
+        cnt += max(0, len / val - 1);
+    }
+    return cnt < 2;
 }
 
 int bs_find(int l, int r)
@@ -28,20 +52,23 @@ int bs_find(int l, int r)
 
 int main(int argc, char const *argv[])
 {
-    cin >> n >> a;
-    int last_pos = 0;
-    int d = INT32_MAX;
+    cin >> n;
+    cin >> a;
+    int last_pos = -1;
+    int d = n + 1;
     for (int i = 1; i <= n; i++)
     {
-        int nowa = a[i] - '0';
+        int nowa = a[i - 1] - '0';
         if (nowa)
         {
-            d = min(d, i - last_pos + 1);
+            if (last_pos != -1)
+                d = min(d, i - last_pos);
             last_pos = i;
         }
     }
 
-    bs_find(0, d + 1);
+    int ans = bs_find(0, d + 1) - 1;
+    cout << ans << endl;
 
     return 0;
 }
